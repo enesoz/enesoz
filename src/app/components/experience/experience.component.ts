f544802b065df15d93b9f07c278875446507b9cf
enesoz <csenes1987@gmail.com>
2026-01-12 21:48:46 +0300

    added group for same company related experiences

--- src/app/components/experience/experience.component.ts
+++ src/app/components/experience/experience.component.ts
@@ -1,7 +1,14 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { Experience } from '../../models/ExperienceInterface';
 import { TranslatePipe } from '../../../services/translate.pipe';
 import { NgForOf, NgIf } from '@angular/common';
+
+interface ExperienceGroup {
+  company: string;
+  logoUrl?: string;
+  location: string;
+  roles: Experience[];
+}
 
 @Component({
   selector: 'app-experience',
@@ -14,6 +21,36 @@ import { NgForOf, NgIf } from '@angular/common';
   ],
   standalone: true
 })
-export class ExperienceComponent {
+export class ExperienceComponent implements OnChanges {
   @Input() experiences: Experience[] = [];
+  groupedExperiences: ExperienceGroup[] = [];
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['experiences'] && this.experiences) {
+      this.groupExperiences();
+    }
+  }
+
+  private groupExperiences(): void {
+    this.groupedExperiences = [];
+    if (!this.experiences.length) return;
+
+    let currentGroup: ExperienceGroup | null = null;
+
+    for (const exp of this.experiences) {
+      if (currentGroup && currentGroup.company === exp.company) {
+        // Add to existing group
+        currentGroup.roles.push(exp);
+      } else {
+        // Start new group
+        currentGroup = {
+          company: exp.company,
+          logoUrl: exp.logoUrl,
+          location: exp.location,
+          roles: [exp]
+        };
+        this.groupedExperiences.push(currentGroup);
+      }
+    }
+  }
 }
